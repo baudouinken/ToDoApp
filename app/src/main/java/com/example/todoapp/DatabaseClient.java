4bc89c9547438cf37f993a456fc10d4150492919
norman-schmidt <60552466+norman-schmidt@users.noreply.github.com>
2020-07-02 17:20:32 +0200

    Reset Room database

--- app/src/main/java/com/example/todoapp/DatabaseClient.java
+++ app/src/main/java/com/example/todoapp/DatabaseClient.java
@@ -2,6 +2,8 @@ package com.example.todoapp;
 
 import androidx.room.Room;
 import android.content.Context;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 public class DatabaseClient {
 
@@ -10,11 +12,22 @@ public class DatabaseClient {
 
     private AppDatabase appDatabase;
 
+//    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+//        @Override
+//        public void migrate(SupportSQLiteDatabase database) {
+//            database.execSQL("ALTER TABLE todos RENAME COLUMN task TO name");
+//        }
+//    };
+
     private DatabaseClient(Context mCtx){
         this.mCtx = mCtx;
 
+        //mCtx.deleteDatabase("todos");
         // Database Name : Todos
-        appDatabase = Room.databaseBuilder(mCtx,AppDatabase.class, "Todos").build();
+        appDatabase = Room.databaseBuilder(mCtx,AppDatabase.class, "todos").build();
+
+        //appDatabase = Room.databaseBuilder(mCtx, AppDatabase.class, "Todos")
+        //        .addMigrations(MIGRATION_1_2).build();
     }
 
     public static synchronized DatabaseClient getInstance(Context mCtx){
