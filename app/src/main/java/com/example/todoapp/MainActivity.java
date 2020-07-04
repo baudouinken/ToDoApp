1f9bed5e824faf679e7b312111189e951b04a22a
norman-schmidt <60552466+norman-schmidt@users.noreply.github.com>
2020-07-04 04:15:31 +0200

    fixed activity lifecycle

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity  extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AddTodoActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
 
